7030e65caa30f40593b8b88c9b74bc409f014ba5
Michael <michael@MichaeldeMacBook-Pro.local>
2018-12-22 11:34:50 +0800

    patten design

---
+++ src/main/java/com/yunxi/springmvc/designpattern/test/patten/filter/Filter.java
@@ -0,0 +1,23 @@
+package com.yunxi.springmvc.designpattern.test.patten.filter;
+
+/**
+ * @Author: 无双老师【云析学院】
+ * @Date: 2018-12-22
+ * @Description: 过滤器接口
+ */
+public interface Filter {
+    /**
+     * 过滤方法
+     */
+    String doFilter(String content);
+
+    /**
+     * 后置处理方法
+     */
+    void doPost(String content);
+
+    /**
+     * 发布时间
+     */
+    void doPublish();
+}
